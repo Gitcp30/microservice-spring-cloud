7e0a33ea40ac99e529f1e4864ca63e317970237c
1808801480@qq.com <1808801480@qq.com>
2017-12-24 12:22:52 +0800

    test

--- microservice-discovery-eureka/src/main/java/com/zzr/MicroserviceDiscoveryEurekaApplication.java
+++ microservice-discovery-eureka/src/main/java/com/zzr/MicroserviceDiscoveryEurekaApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.function.DoubleToIntFunction;
+
 
 @SpringBootApplication
 @EnableEurekaServer
 public class MicroserviceDiscoveryEurekaApplication {
 
     public static void main(String[] args) {
+        System.out.print(11);
         SpringApplication.run(MicroserviceDiscoveryEurekaApplication.class, args);
     }
 }
